a558dda9ea446f24272e9d9c5bc1c22b900ed606
PRACHI PARAB <prachi.parab.dev4@gmail.com>
2026-02-08 10:13:02 +0530

    Add files via upload

---
+++ Array/inptOutpt.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class inptOutpt {
+    public static void main (String[] args) {
+        int marks[] = new int[100];
+
+        //input in array
+        Scanner sc = new Scanner(System.in);
+        //int phy;
+        //phy = sc.nextInt();
+
+        //marks[0] = sc.nextInt();      //phy
+        //marks[1] = sc.nextInt();      //chem
+        //marks[2] = sc.nextInt();      //math
+
+        System.out.println("lenghth of the array is : " + marks.length);
+
+
+        //output in array
+        //System.out.println("phy: " + marks[0]);
+        //System.out.println("chem: " + marks[1]);
+        //System.out.println("math: " + marks[2]);
+
+       //int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+       //System.out.println("percentage:" + percentage + "%");
+
+    }
+}
