4790ae3605522b6125092e53a60a829b5c57e8d3
PRACHI PARAB <prachi.parab.dev4@gmail.com>
2026-02-20 09:53:25 +0530

    Create sumof2num.java

---
+++ Logic building/sumof2num.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter 1st number:");
+        int num1 = sc.nextInt();
+        
+        System.out.println("Enter 2nd number:");
+        int num2 = sc.nextInt();
+        
+        int sum = num1 + num2;
+        System.out.println("Sum :" + sum);
+        
+    }
+}
