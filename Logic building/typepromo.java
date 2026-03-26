2492687cb3e4892dcac039b86c4c61b2de1d3583
PRACHI PARAB <prachi.parab.dev4@gmail.com>
2026-03-26 21:42:19 +0530

    Create typepromo.java

---
+++ Logic building/typepromo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class typePromo {
+    public static void main(String args[]) {
+
+        char a = 'a';
+        char b = 'b';
+
+        System.out.println((int) (b)); // 98
+        System.out.println((int) (a)); // 97
+        System.out.println(b - a);     // 1
+    }
+}
